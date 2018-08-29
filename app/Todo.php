223b054c3a3c62fd84034f33eaa0bfe6e2be4866
Vinh <levinh090496@gmail.com>
2018-08-29 22:59:34 +0700

    third commit

--- app/Todo.php
+++ app/Todo.php
@@ -9,7 +9,7 @@ class Todo extends Model
 	//protected $table = 'todos';
 
     //protected $fillable = ['id','todo','created_at','updated_at'];
-    protected $fillable = ['todo'];
+    protected $fillable = ['title','content','time','location'];
 
     public static function getAll(){
     	return Todo::get();
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,14 @@
 
 // Route::get('/', function () {
 //     return view('welcome');
-	Route::get('todos','TodoController@index');
-	Route::get('todos/create','TodoController@create');
-	Route::get('todos/{id}','TodoController@show');
-	Route::post('todos','TodoController@store');
-	Route::delete('todos/{id}','TodoController@destroy');
-	Route::get('todos/{id}/edit','TodoController@edit');
-	Route::put('todos/{id}', 'TodoController@update');
+	// Route::get('todos','TodoController@index');
+	// Route::get('todos/create','TodoController@create');
+	// Route::get('todos/{id}','TodoController@show');
+	// Route::post('todos','TodoController@store');
+	// Route::delete('todos/{id}','TodoController@destroy');
+	// Route::get('todos/{id}/edit','TodoController@edit');
+	// Route::put('todos/{id}', 'TodoController@update');
+Route::resource('todos-ajax','TodoAjaxController');
 
 	
 
